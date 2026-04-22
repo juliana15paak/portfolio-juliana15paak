d16f02100407de5d8257b06dd57ae51296d6cd09
juliana15paak <fvjulianaasilva976@gmail.com>
2026-04-22 18:41:53 -0300

    feat: add carrossel para timeline

--- script.js
+++ script.js
@@ -8,3 +8,13 @@ function scrollCarousel(direction) {
     behavior: 'smooth'
   });
 }
+function scrollTimeline(direction) {
+  const container = document.getElementById('timelineList');
+  // Pega a largura de um card + o gap de 2rem (32px)
+  const cardWidth = container.querySelector('.card-timeline').offsetWidth + 32;
+  
+  container.scrollBy({
+    left: direction * cardWidth,
+    behavior: 'smooth'
+  });
+}
