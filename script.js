2dfb19d019905b0216f16cf3a3d40751b9e6c86c
juliana15paak <fvjulianaasilva976@gmail.com>
2026-04-22 20:52:33 -0300

    feat: ajusta mini carrossel para fotos

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//=========================== CARROSSEL PROJETOS ==========================
 function scrollCarousel(direction) {
   const container = document.getElementById('projectsList');
   // Pega a largura do primeiro card + o gap (2rem = 32px aproximadamente)
@@ -8,6 +9,7 @@ function scrollCarousel(direction) {
     behavior: 'smooth'
   });
 }
+//=========================== CARROSSEL TIMELINE ==========================
 function scrollTimeline(direction) {
   const container = document.getElementById('timelineList');
   // Pega a largura de um card + o gap de 2rem (32px)
@@ -17,4 +19,26 @@ function scrollTimeline(direction) {
     left: direction * cardWidth,
     behavior: 'smooth'
   });
+}
+//=========================== CARROSSEL DE MINI FOTOS ==========================
+let currentIdx = 0;
+
+function moveMini(direction, trackId) {
+  const track = document.getElementById(trackId);
+  const container = track.parentElement;
+  const images = track.querySelectorAll('img');
+  const dots = container.querySelectorAll('.dot');
+  
+  let index = parseInt(track.dataset.index || 0);
+  
+  index += direction;
+
+  if (index >= images.length) index = 0;
+  if (index < 0) index = images.length - 1;
+
+  track.style.transform = `translateX(-${index * 400}px)`;
+  track.dataset.index = index;
+
+  dots.forEach(dot => dot.classList.remove('active'));
+  dots[index].classList.add('active');
 }
